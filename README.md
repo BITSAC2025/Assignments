6e18ab3e85d7010dc5047b2444da2e08c8b1c558
kisslune <youthyoungray@gmail.com>
2025-10-07 18:55:21 +1100

    cmakelist

--- README.md
+++ README.md
@@ -8,3 +8,6 @@
 - After running `./build.sh`, an executable is expected to be in each corresponding subdirectory.
 
 - Then, you can use the test cases in `Assignment.../Test-Cases` to check whether you code is correct.
+
+#### Note:
+Every time you upload your assignments, please attach your **NAME** and **STUDENT ID** to this README file!
--- build.sh
+++ build.sh
@@ -2,8 +2,17 @@
 
 ARGS="$@"
 
-rm -rf build/
+if [ -n "${SVF_DIR}" ] && [ -d "${SVF_DIR}/llvm-16.0.0.obj" ]; then
+  export LLVM_DIR="${SVF_DIR}/llvm-16.0.0.obj"
+  echo "LLVM_DIR: $LLVM_DIR"
+elif [ -n "${LLVM_DIR}" ] && [ -d "${LLVM_DIR}" ]; then
+  echo "LLVM_DIR: $LLVM_DIR"
+else
+  echo "LLVM_DIR not found!"
+  exit 1
+fi
 
+rm -rf build/
 mkdir build && cd build/
 
 if [ -n "$ARGS" ]; then
